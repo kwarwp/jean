d1f25cd3e60094f6a5119100dd2ccb380e478c4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-18 11:17:24 -0300

    Created roxanne/__log__.py at Timestamp: 2018-07-18 14:17:24

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jul 18 2018 11:17:24.441 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  R_OESTE = “https://i.imgur.com/XJXjA9r.jpg”
+             ^
+SyntaxError: invalid syntax
+'''},
