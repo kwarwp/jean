7dd53386c11e45a8bb96c2dd52707a7be4368a92
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 11:38:19 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-07-23 14:38:19

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -157,3 +157,21 @@ NameError: name 'museu' is not defined
     sala_1.norte.meio = sala_2.norte
 NameError: name 'sala_2' is not defined
 '''},
+{'date': 'Mon Jul 23 2018 11:35:19.936 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    Museu()
+  module <module> line 60
+    sala_1.norte.meio = sala_2.norte
+NameError: name 'sala_2' is not defined
+'''},
