1e5c030aca252f16f6ae059b8a4c32da738958b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 10:05:25 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-23 13:05:25

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,1 +1,39 @@
 # jean.roxanne.main.py
+
+from _spy.vitollino.main import Cena, Texto, Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+STYLE ["width"] = 500
+
+R_OESTE = “https://i.imgur.com/XJXjA9r.jpg”
+R_LESTE = “https://i.imgur.com/rHzbmtM.jpg”
+R_NORTE = “https://i.imgur.com/IPa06hM.jpg”
+C1_LESTE = “https://i.imgur.com/YYuRyQR.jpg”
+C1_OESTE = “https://i.imgur.com/ryMwKc8.jpg”
+C1_NORTE = “https://i.imgur.com/uTIO2tN.jpg”
+C1_SUL = “https://i.imgur.com/1fMjUFO.jpg”
+C2_NORTE = “https://i.imgur.com/FlwvTzi.jpg”
+C2_LESTE = “https://i.imgur.com/9uub4QD.jpg”
+C2_OESTE = “https://i.imgur.com/vJZ7pKJ.jpg”
+C2*_NORTE = “https://i.imgur.com/hxZUIOf.jpg”
+C2*_LESTE = “https://i.imgur.com/MCEVdWU.jpg”
+C2*_OESTE = “https://i.imgur.com/ozE3Qhg.jpg”
+C3_NORTE = “https://i.imgur.com/rRzsYT8.jpg”
+C3_LESTE = “https://i.imgur.com/uy7BGaZ.jpg”
+C3_OESTE = “https://i.imgur.com/ZxC1zBd.jpg”
+C4_NORTE = “https://i.imgur.com/ZX7ksna.jpg”
+C4_LESTE = “https://i.imgur.com/31TCLek.jpg”
+C4_OESTE = “https://i.imgur.com/xw3dplb.jpg”
+C5_NORTE = “https://i.imgur.com/BoFO4Dw.jpg”
+C5_OESTE = “https://i.imgur.com/13RbfOr.jpg”
+C5_LESTE =  “https://i.imgur.com/VSU2JRi.jpg”
+C6_NORTE = “https://i.imgur.com/fQjnFkR.jpg”
+C6_LESTE = “https://i.imgur.com/aqAllvM.jpg”
+C6_OESTE = “https://i.imgur.com/GonsqOz.jpg”
+C7_NORTE = “https://i.imgur.com/GwKkapl.jpg”
+C7_LESTE = “https://i.imgur.com/AXT4Zcl.jpg”
+C9_OESTE = “https://i.imgur.com/PAk5ZH1.jpg”
+C9_SUL = “https://i.imgur.com/2kCYcjs.jpg”
+C9_NORTE = “https://i.imgur.com/0DAX1Yq.jpg”
+C9_LESTE = “https://i.imgur.com/Clwe0iK.jpg”
+C9_OESTE = “https://i.imgur.com/cOVZAln.jpg”
+C9_SUL = “https://i.imgur.com/pIRvnJS.jpg”
