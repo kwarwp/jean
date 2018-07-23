4649be1527799a026adcfa698ba22d729b43b59e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 15:54:06 -0300

    Saved stacy/main.py at Timestamp: 2018-07-23 18:54:06

--- stacy/main.py
+++ stacy/main.py
@@ -1,4 +1,5 @@
 # jean.stacy.main.py
+WHITE = "https://i.imgur.com/opHOSiE.jpg"
 PAREDES = dict(
 C0_OESTE = "https://i.imgur.com/XJXjA9r.jpg",
 C0_LESTE = "https://i.imgur.com/rHzbmtM.jpg",
@@ -34,12 +35,68 @@ C9_LESTE = "https://i.imgur.com/Clwe0iK.jpg",
 C9_OESTE = "https://i.imgur.com/cOVZAln.jpg",
 C9_SUL = "https://i.imgur.com/pIRvnJS.jpg"
 )
-from _spy.vitollino.main import Sala, STYLE, NADA
+from _spy.vitollino.main import Sala, STYLE, NADA, Elemento, INVENTARIO, PSTYLE, EIMGSTY, NS
+from browser import window, html
+EDTST = {'position': 'relative', 'padding': '10px', 'margin': '0px',
+         'width': '100%', 'resize': 'none', 'borderColor': 'darkslategrey',
+         'color': 'navajowhite', 'border': 0, 'background-color': 'rgba(76, 175, 80, 0.3)'}
 
-STYLE["width"] = 850
+STYLE["width"] = 1000
 STYLE["height"] = "650px"
 RUMOS = "NORTE LESTE SUL OESTE".split()
 CART = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # [(i, j) for j, i in CART]
+
+CODE_0 = """
+from _spy.vitollino.main import Cena
+C9_OESTE = "https://i.imgur.com/cOVZAln.jpg",
+cena = Cena(C9_OESTE)
+cena.vai()
+"""
+
+class Codigo(Elemento):
+    """
+    Um objeto de interação que é representado por uma imagem em uma cena.
+            papel = Elemento(
+             img="papel.png", tit="caderno de notas",
+             vai=pega_papel, style=dict(left=350, top=550, width=60))
+    :param img: URL de uma imagem
+    :param vai: função executada quando se clica no objeto
+    :param style: dicionário com dimensões do objeto {"left": ..., "top": ..., width: ..., height: ...}
+    :param tit: Texto que aparece quando se passa o mouse sobre o objeto
+    :param alt: Texto para leitores de tela
+    :param cena: cena alternativa onde o objeto vai ser colocado
+    :param score: determina o score para este elemento
+    :param kwargs: lista de parametros nome=URL que geram elementos com este nome e a dada imagem
+    """
+    def __init__(self, codigo="", cena=INVENTARIO, img="", vai=None, style=NS):
+        self.img = img
+        self.vai = vai if vai else lambda _=0: None
+        self.cena = cena
+        self.opacity = 0
+        self.style = dict(**PSTYLE)
+        # self.style["min-width"], self.style["min-height"] = w, h
+        self.style.update(**style)
+        self.elt = html.DIV(style=self.style)
+        self.xy = (-111, -111)
+        istyle = dict(EIMGSTY)
+        istyle.update(opacity=0.3)
+        if img:
+            self.img = html.IMG(src=img, style=istyle)  # width=self.style["width"])
+            self.elt <= self.img
+        self.elt.onclick = self._click
+        self.scorer = dict(ponto=1, valor=cena.nome, carta=img, casa=self.xy, move=None)
+        self._area = html.TEXTAREA(codigo, autofocus=True, style=dict(position='absolute', top=0))
+        self.elt <= self._area
+        
+        self.__area = window.CodeMirror.fromTextArea(
+            self._area, dict(
+                mode="python", theme="solarized", lineNumbers=False, indentUnit=4,
+                tabSize=4, smartIndent=False, matchBracket=True, indentWithTabs=False))
+
+
+        _ = self.entra(cena) if cena and (cena != INVENTARIO) else None
+        self._area.value = codigo
+        self.__area.focus()
 
 
 class Labirinto:
@@ -73,9 +130,11 @@ class Museu:
         museu = {"sala_{}".format(indice): Sala(
             *[PAREDES["C{}_{}".format(indice, rumo)] for rumo in RUMOS], nome="sala_{}".format(indice))
         for indice in range(10)}
-        museu["sala_0"].norte.vai()
         mapa = [[museu["sala_{}".format((j+i) %10)] for i in range(4)] for j in range(0, 16, 4)]
         Labirinto.m(mapa)
+        entrada = museu["sala_0"].norte
+        cod = Codigo(cena=entrada, codigo=CODE_0, img=WHITE, style=dict(width=400, height="300px", left=500, top=100))
+        entrada.vai()
         
         
         
