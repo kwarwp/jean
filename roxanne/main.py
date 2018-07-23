7056c7885402fcef226347b1914fa8e17c6ba246
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 10:44:36 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-23 13:44:35

--- roxanne/main.py
+++ roxanne/main.py
@@ -41,8 +41,8 @@ class Museu:
         R_SUL = R_OESTE
         sala_0 = Sala(R_NORTE, R_LESTE,R_SUL, R_OESTE)
         sala_1 = Sala(C1_NORTE, C1_LESTE, C1_OESTE)
-        sala_0.norte = sala_1.norte
-        sala_0.norte.vai = sala_1.norte.vai()
+        sala_0.norte.meio = sala_1.norte
+        sala_0.norte.vai()
         
         
 Museu()
