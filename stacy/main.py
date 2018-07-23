1e494b0e71c2c1714f7363e84a22b0756cdda539
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 13:18:07 -0300

    Saved stacy/main.py at Timestamp: 2018-07-23 16:18:07

--- stacy/main.py
+++ stacy/main.py
@@ -34,11 +34,39 @@ C9_LESTE = "https://i.imgur.com/Clwe0iK.jpg",
 C9_OESTE = "https://i.imgur.com/cOVZAln.jpg",
 C9_SUL = "https://i.imgur.com/pIRvnJS.jpg"
 )
-from _spy.vitollino.main import Sala, STYLE
+from _spy.vitollino.main import Sala, STYLE, NADA
 
 STYLE["width"] = 850
 STYLE["height"] = "650px"
 RUMOS = "NORTE LESTE SUL OESTE".split()
+CART = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # [(i, j) for j, i in CART]
+
+
+class Labirinto:
+
+    @staticmethod
+    def m(cenas):
+        def valid(cns, jj, ii, m, n):
+            return 0 <= jj + m < len(cns) and 0 <= ii + n < len(cns[jj + m]) and cns[jj + m][ii + n]
+
+        def vizinhos(jj, ii, cns=cenas):
+            return [(kk, cns[jj + m][ii + n]) for kk, (m, n) in enumerate(CART) if valid(cns, jj, ii, m, n)]
+            # return [(kk, cns[jj + m][ii + n]) for kk, (m, n) in enumerate(CART)
+            #  if 0 <= jj + m < len(cns) and 0 <= ii + n < len(cns[jj+m])and cns[jj + m][ii + n]]
+
+        for j, linha in enumerate(cenas):
+            if isinstance(linha, list):
+                for i, centro in enumerate(linha):
+                    if not isinstance(centro, Sala):
+                        continue
+                    for k, sala in vizinhos(j, i):
+                        if not isinstance(sala, Sala):
+                            continue
+                        centro.cenas[k].meio = sala.cenas[k]
+                        indice_oposto = (k + 2) % 4
+                        sala.cenas[indice_oposto].meio = centro.cenas[indice_oposto]
+
+
 class Museu:
     def __init__(self):
         [PAREDES.update({"{}_SUL".format(sala[:2]): imagem}) for sala, imagem in PAREDES.items() if "OESTE" in sala]
@@ -46,6 +74,8 @@ class Museu:
             *[PAREDES["C{}_{}".format(indice, rumo)] for rumo in RUMOS], nome="sala_{}".format(indice))
         for indice in range(10)}
         museu["sala_0"].norte.vai()
+        mapa = [[museu[(j %10)+i] for i in range(4)] for j in range(0, 16, 4)]
+        Labirinto.m(mapa)
         
         
         
