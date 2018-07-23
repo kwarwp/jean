25f376d8e3d8e8fdc4587b4b83997c6b627da1b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 14:57:48 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-23 17:57:48

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -95,3 +95,25 @@ NameError: name 'score' is not defined
 'error': '''
 Exception: <TypeError: textarea.parentNode is null>
 '''},
+{'date': 'Mon Jul 23 2018 14:54:48.983 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 134
+    Museu()
+  module <module> line 128
+    cod = Codigo(cena=entrada, codigo=CODE_0, style=dict(width=300, height="400px", left=400, top=100))
+  module <module> line 91
+    _ = self.entra(cena) if cena and (cena != INVENTARIO) else None
+  module _spy.vitollino.main line 453
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'Codigo' object has no attribute 'scorer'
+'''},
