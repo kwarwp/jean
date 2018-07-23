a2de5fef47abef17c2369bf7de8bea2a1269bcf8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 13:17:56 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-23 16:17:55

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -35,3 +35,21 @@ AttributeError: 'dict' object has no attribute 'put'
     museu = {"sala_{}".format(indice): Sala(
 NameError: name 'RUMOS' is not defined
 '''},
+{'date': 'Mon Jul 23 2018 13:14:56.255 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 83
+    Museu()
+  module <module> line 77
+    mapa = [[museu[(j %10)+i] for i in range(4)] for j in range(0, 16, 4)]
+KeyError: 0
+'''},
