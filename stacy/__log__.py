e01fb1b15d94e097a31c5283cbe38b28b55dd8a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 14:54:40 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-23 17:54:40

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -71,3 +71,23 @@ KeyError: 0
     mapa = [[museu["sala_{}".format((j %10)+i)] for i in range(4)] for j in range(0, 16, 4)]
 KeyError: sala_10
 '''},
+{'date': 'Mon Jul 23 2018 14:51:40.777 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    Museu()
+  module <module> line 127
+    cod = Codigo(cena=entrada, codigo=CODE_0, style=dict(width=300, height="400px", left=400, top=100))
+  module <module> line 71
+    self._auto_score = self.score if score else self._auto_score
+NameError: name 'score' is not defined
+'''},
