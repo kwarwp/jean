3c732bf949c1253f44faa5fdbfa7b819af974534
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-06 19:44:46 -0300

    Saved sara/__log__.py at Timestamp: 2018-08-06 22:44:45

--- sara/__log__.py
+++ sara/__log__.py
@@ -51,3 +51,25 @@ NameError: name 'sala_1' is not defined
     Museu.CUR.instrumenta(self)
 AttributeError: 'Cena' object has no attribute 'instrumenta'
 '''},
+{'date': 'Mon Aug 06 2018 19:40:54.895 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    cena.vai()
+  module stacy.main line 103
+    Museu.CUR.instrumenta(self)
+  module stacy.main line 99
+    self.cod.entra(cena)
+  module _spy.vitollino.main line 456
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'Museu' object has no attribute 'nome'
+'''},
