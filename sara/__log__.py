883b105e56bd90e967858e6697570c4fd373e33f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-06 19:40:06 -0300

    Saved sara/__log__.py at Timestamp: 2018-08-06 22:40:06

--- sara/__log__.py
+++ sara/__log__.py
@@ -33,3 +33,21 @@ TypeError: 'module' object is not callable
     TUT=[
 NameError: name 'sala_1' is not defined
 '''},
+{'date': 'Mon Aug 06 2018 19:36:15.276 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    cena.vai()
+  module stacy.main line 102
+    Museu.CUR.instrumenta(self)
+AttributeError: 'Cena' object has no attribute 'instrumenta'
+'''},
