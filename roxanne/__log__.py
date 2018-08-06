9ae6897b116f33e11ff35bbd622c8e62ef7cb2a2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-06 17:32:01 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-06 20:32:01

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -260,3 +260,34 @@ NameError: name 'sala_8' is not defined
     mapa = Labirinto.m(
 NameError: name 'sala_8' is not defined
 '''},
+{'date': 'Mon Aug 06 2018 17:31:45.946 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 61
+    from _spy.vitollino.main import Sala, STYLE, codigo
+ImportError: cannot import name 'codigo'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 61
+    from _spy.vitollino.main import Sala, STYLE, codigo
+'''},
