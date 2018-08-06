5c75a48448428a2f97bf206a1e1667daa422d345
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-06 19:00:38 -0300

    Saved stacy/__log__.py at Timestamp: 2018-08-06 22:00:38

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -231,3 +231,21 @@ NameError: name 'window' is not defined
     entrada.direita = Cena(vai=mostra_codigo)
 NameError: name 'Cena' is not defined
 '''},
+{'date': 'Mon Aug 06 2018 18:56:47.514 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 104
+    Museu()
+  module <module> line 94
+    cod = Codigo(cena=entrada, topo=topo, codigo=CODE_0, style=dict(width=400, height="250px", left=500, top=100))
+NameError: name 'Codigo' is not defined
+'''},
