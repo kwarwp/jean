494d800547fb38e2906d610d4c0e4a192a26fbe9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-06 19:01:53 -0300

    Saved stacy/__log__.py at Timestamp: 2018-08-06 22:01:53

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -249,3 +249,23 @@ NameError: name 'Cena' is not defined
     cod = Codigo(cena=entrada, topo=topo, codigo=CODE_0, style=dict(width=400, height="250px", left=500, top=100))
 NameError: name 'Codigo' is not defined
 '''},
+{'date': 'Mon Aug 06 2018 18:58:02.85 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 104
+    Museu()
+  module <module> line 95
+    entrada.vai()
+  module <module> line 97
+    self.corrente.vai()
+AttributeError: 'Cena' object has no attribute 'corrente'
+'''},
