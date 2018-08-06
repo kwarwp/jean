12c58c2d59f3ffd724e086eac02fab689320e0d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-06 18:36:26 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-06 21:36:26

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -342,3 +342,21 @@ SyntaxError: invalid syntax
     MENSAGENS=[
 NameError: name 'sala_0' is not defined
 '''},
+{'date': 'Mon Aug 06 2018 18:36:11.196 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 136
+    Museu()
+  module <module> line 126
+    MENSAGENS=[
+AttributeError: 'Sala' object has no attribute 'este'
+'''},
