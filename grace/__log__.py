f2df2ae7f75ba6b98e475f8b0c61147773e8ae9f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-09 20:55:55 -0300

    Created grace/__log__.py at Timestamp: 2018-08-09 23:55:55

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Aug 09 2018 20:55:53.768 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 196
+  ain.py
+  ^
+IndentationError: unexpected indent
+'''},
