3693c053e02be0cae653db6d5713232b4dbecdd2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-09 21:17:23 -0300

    Saved grace/__log__.py at Timestamp: 2018-08-10 00:17:23

--- grace/__log__.py
+++ grace/__log__.py
@@ -24,3 +24,21 @@ IndentationError: unexpected indent
     sala_0 = Sala(R_NORTE, R_LESTE, R_SUL, R_OESTE)
 NameError: name 'R_OESTE' is not defined
 '''},
+{'date': 'Thu Aug 09 2018 21:17:22.791 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 202
+    Museu()
+  module <module> line 121
+    dinotexto = Texto(sala_2.leste, "e ai,colega, tudo tranks?")
+NameError: name 'Texto' is not defined
+'''},
