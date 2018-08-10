3572ded86a82191a8cd021a9e197c0ab7fc459a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-09 21:53:18 -0300

    Saved grace/__log__.py at Timestamp: 2018-08-10 00:53:17

--- grace/__log__.py
+++ grace/__log__.py
@@ -49,3 +49,10 @@ NameError: name 'Texto' is not defined
                                                                                                                                                                                                                                                                    ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Aug 09 2018 21:53:17.5 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 140
+  [sala_2.leste,"vamos aprender a associar um elemento e um texto? É so seguir as instruçoes",  "A primeira coisa a se fazer é importar do vitollino:Elemento e Texto(caso não tenha no seu código).Vai ficar assim:from _spy.vitollino.main import Elemento, Cena.
+                                                                                                                                                                                                                                                                   ^
+SyntaxError: EOL while scanning string literal
+'''},
