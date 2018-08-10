84db37b7903a86706d82bae3e2d6d97f99a336ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-10 10:13:12 -0300

    Saved natalia/__log__.py at Timestamp: 2018-08-10 13:13:12

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Aug 10 2018 10:12:56.6 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  if __name__ == '__main__':
+  ^
+IndentationError: expected an indented block
+'''},
