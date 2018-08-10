decd95c5fb595fc7bf323518c8ae96a20ddc6442
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-10 10:41:44 -0300

    Saved grace/__log__.py at Timestamp: 2018-08-10 13:41:43

--- grace/__log__.py
+++ grace/__log__.py
@@ -91,3 +91,21 @@ SyntaxError: EOL while scanning string literal
                                                          ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Fri Aug 10 2018 10:41:26.889 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 219
+    Museu()
+  module <module> line 126
+    sala2_leste.vai()
+NameError: name 'sala2_leste' is not defined
+'''},
