d241c5fc125cfd94e3e02742bbf0659eeb38744c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-10 10:13:15 -0300

    Created natalia/main.py at Timestamp: 2018-08-10 13:13:15

---
+++ natalia/main.py
@@ -0,0 +1,18 @@
+# jean.natalia.main.py
+from _spy.vitollino.main import Sala, STYLE, Codigo, Labirinto
+C9_OESTE = "https://i.imgur.com/cOVZAln.jpg"
+C9_SUL = "https://i.imgur.com/pIRvnJS.jpg"
+
+STYLE["width"] = 850
+STYLE["height"] = "650px"
+
+class Labirinto:
+    def __init__(self):
+    
+    
+    
+    
+if __name__ == '__main__':
+    Labirinto()
+
+
