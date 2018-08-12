2a3eae8d55fee704093213930099df722a46f94b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 20:51:31 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-12 23:51:31

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -17,3 +17,21 @@
     interrogacao_ = Elemento(interrogacao,cena= INVENTARIO, tit = "Help", 
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Sun Aug 12 2018 20:51:30.672 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 201
+    Museu()
+  module <module> line 119
+    interrogacao_ = Elemento(interrogacao,cena= INVENTARIO, tit = "Help", 
+NameError: name 'interrogacao' is not defined
+'''},
