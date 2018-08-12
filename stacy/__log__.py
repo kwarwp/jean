8aca50b9818cd212b72d28de024c451ab039c9e9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 20:37:07 -0300

    Saved stacy/__log__.py at Timestamp: 2018-08-12 23:37:07

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -311,3 +311,27 @@ AttributeError: 'Cena' object has no attribute 'corrente'
     self.corrente.vai()
 AttributeError: 'Cena' object has no attribute 'corrente'
 '''},
+{'date': 'Sun Aug 12 2018 20:37:01.205 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 112
+    Museu()
+  module <module> line 97
+    entrada.vai()
+  module <module> line 104
+    Museu.CUR.instrumenta(self)
+  module <module> line 100
+    self.cod.entra(cena)
+  module _spy.vitollino.main line 459
+    cena <= self
+TypeError: '<=' not supported between instances of 'Museu' and 'Codigo'
+'''},
