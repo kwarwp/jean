93cb01dc72d9133b50edda8c144dc28257d219d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 20:51:12 -0300

    Saved kellee/main.py at Timestamp: 2018-08-12 23:51:11

--- kellee/main.py
+++ kellee/main.py
@@ -58,7 +58,7 @@ C9_NORTE = "https://i.imgur.com/0DAX1Yq.jpg"
 C9_LESTE = "https://i.imgur.com/Clwe0iK.jpg"
 C9_OESTE = "https://i.imgur.com/cOVZAln.jpg"
 C9_SUL = "https://i.imgur.com/pIRvnJS.jpg"
-interrogacao="https://publicdomainvectors.org/photos/primary-gnome-question.png"
+interrogacao_="https://publicdomainvectors.org/photos/primary-gnome-question.png"
 from _spy.vitollino.main import Sala, STYLE, Codigo, INVENTARIO, Elemento
 
 STYLE["width"] = 850
