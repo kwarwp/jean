107f3533a9799af78438323b98cb8dcb3836d04d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 21:22:11 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-13 00:22:11

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -111,3 +111,21 @@ NameError: name 'html' is not defined
     a = html.A("&times;", Class="close", href="#")         
 NameError: name 'html' is not defined
 '''},
+{'date': 'Sun Aug 12 2018 21:22:10.229 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    class Codigo(Code):
+  module <module> line 67
+    def __init__(self, codigo="", topo="", cena=INVENTARIO, img="", vai=None, style=NS):
+NameError: name 'NS' is not defined
+'''},
