b8ab098add89732907606a3fda32e35509064a05
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 18:54:27 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 21:54:27

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -318,3 +318,21 @@ SyntaxError: invalid syntax
                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 13 2018 18:54:09.692 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    class Elemento(Element):
+  module <module> line 17
+    def __init__(self, img="", vai=None, style=NDCT, tit="", alt="",
+NameError: name 'NDCT' is not defined
+'''},
