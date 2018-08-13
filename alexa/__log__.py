0b76d8a465778733f631baff5343888528d81aa1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 19:47:02 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 22:47:02

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -659,3 +659,23 @@ AttributeError: 'Elemento' object has no attribute 'nome'
     self.score.update(score)
 AttributeError: 'method' object has no attribute 'update'
 '''},
+{'date': 'Mon Aug 13 2018 19:46:44.364 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 173
+    natureza()    
+  module <module> line 168
+    abelha = Elemento(ABELHA, tit = "abelha", drag=True,
+  module <module> line 43
+    self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
+AttributeError: 'Elemento' object has no attribute 'nome'
+'''},
