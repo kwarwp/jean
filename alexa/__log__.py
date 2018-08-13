86ab1e2495af561c0413a38e4be38282df4894fb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 16:27:55 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 19:27:55

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -207,3 +207,21 @@ AttributeError: 'Folha' object has no attribute 'mouse_over'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Aug 13 2018 16:27:37.940 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 240
+    trigonometria().norte.vai()
+  module <module> line 226
+    vdgball = Elemento(ball, tit = "microscope", drag=True,
+TypeError: 'module' object is not callable
+'''},
