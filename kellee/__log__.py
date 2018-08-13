e7fd23ec9c62c97e2b8717ec3e15a5e1f8d0bfe9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 21:19:29 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-13 00:19:28

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -71,3 +71,23 @@ AttributeError: 'Inventario' object has no attribute 'pega'
     def __init__(self, codigo="", topo="", cena=INVENTARIO, img="", vai=None, style=NS):
 NameError: name 'NS' is not defined
 '''},
+{'date': 'Sun Aug 12 2018 21:19:28.135 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 211
+    Museu()
+  module <module> line 207
+    [Codigo(cena = a,topo = b ,codigo= c, style= STYLE) for a, b , c in MENSAGENS]
+  module <module> line 69
+    a = html.A("&times;", Class="close", href="#")         
+NameError: name 'html' is not defined
+'''},
