5c1f28dd32690e650aed0a419851f2bac29ad0e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 23:00:04 -0300

    Created callie/__log__.py at Timestamp: 2018-08-13 02:00:04

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Aug 12 2018 22:59:58.209 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 60
+  def mouse_over(self, ev):
+  ^
+IndentationError: unexpected indent
+'''},
