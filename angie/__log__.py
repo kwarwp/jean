44b3ca7a30d70d69f0badb3364c177fa78f8b120
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 13:23:23 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-13 16:23:23

--- angie/__log__.py
+++ angie/__log__.py
@@ -57,3 +57,21 @@ Sala3D(IMG_LIST)
                                                              ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Mon Aug 13 2018 13:19:20.999 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 87
+    Sala3D(IMG_LIST)
+  module <module> line 77
+    local_light(pos=(0,0,4))
+NameError: name 'local_light' is not defined
+'''},
