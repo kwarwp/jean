54aa855a4182027758b6f4014d39487a70b46706
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 18:59:36 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 21:59:36

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -482,3 +482,19 @@ AttributeError: 'function' object has no attribute 'vai'
     natureza.vai()    
 AttributeError: 'function' object has no attribute 'vai'
 '''},
+{'date': 'Mon Aug 13 2018 18:59:18.898 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 169
+    natureza.vai()    
+AttributeError: 'function' object has no attribute 'vai'
+'''},
