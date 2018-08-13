d5d8dc4e81bdc8433f9683fce8a239fd61d3d305
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 09:09:39 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 12:09:38

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -66,3 +66,21 @@ NameError: name 'Suporte' is not defined
     Suporte(self, "folha" + fl, pos//4, pos%4)
 NameError: name 'Suporte' is not defined
 '''},
+{'date': 'Mon Aug 13 2018 09:10:25.792 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 133
+    Bloco(FLORESTA)
+  module <module> line 119
+    Suporte(self, "folha" + fl, pos//4, pos%4)
+NameError: name 'Suporte' is not defined
+'''},
