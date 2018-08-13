4b1ab159e95f94e09b151ac24b04ac156e6d14e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 11:21:56 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-13 14:21:56

--- angie/__log__.py
+++ angie/__log__.py
@@ -22,3 +22,19 @@ IndentationError: expected an indented block
     Sala3D()
 TypeError: __init__() missing 1 positional argument: ['img_list']
 '''},
+{'date': 'Mon Aug 13 2018 11:17:53.983 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    doc['pydiv'].html = ''
+NameError: name 'doc' is not defined
+'''},
