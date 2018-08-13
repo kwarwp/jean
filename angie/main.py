a4fb4f27a84dc31fbf2c1d4df9308c1e0ea1e623
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 13:15:04 -0300

    Saved angie/main.py at Timestamp: 2018-08-13 16:15:04

--- angie/main.py
+++ angie/main.py
@@ -77,7 +77,7 @@ class Sala3D:
         for direcao, parede in enumerate(img_list):
             parede_ = box(pos=(2*POS[direcao][0], 2*POS[direcao][1], 0), size=(0.2, 2, 2), texture=dict(file=parede, place=["right"]))
             
-            parede_.rotate(angle=direcao*pi/2.0, axis=vec(0,0,1))
+            parede_.rotate(angle=direcao*pi/2.0, axis=vec(0,-1,0))
 
 class Museu:
     def __init__(self):  
