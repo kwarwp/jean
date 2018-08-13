3846b70d83576bd25e558d64e150ff211e4910f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 22:28:22 -0300

    Saved libby/__log__.py at Timestamp: 2018-08-13 01:28:22

--- libby/__log__.py
+++ libby/__log__.py
@@ -19,3 +19,21 @@
     def rp(cod, keys=PKEYS[:], mark='<span class="hljs-keyword">{}</span>'):
 NameError: name 'PKEYS' is not defined
 '''},
+{'date': 'Sun Aug 12 2018 22:28:18.497 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 106
+    Museu()
+  module <module> line 98
+    ex.entra(sala_0)
+NameError: name 'ex' is not defined
+'''},
