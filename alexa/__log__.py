4fc76f4e16e67683350af27e71405ac417a13849
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 15:04:55 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 18:04:55

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -144,3 +144,19 @@ AttributeError: 'Folha' object has no attribute 'mouse_over'
     self.folha.onmouseover = self.mouse_over
 AttributeError: 'Folha' object has no attribute 'mouse_over'
 '''},
+{'date': 'Mon Aug 13 2018 15:04:37.952 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    gogh = Cena(GOGH)
+NameError: name 'GOGH' is not defined
+'''},
