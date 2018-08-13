edae8c09835860eafc4e391c07f1bd5afee83f3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 14:06:10 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-13 17:06:10

--- angie/__log__.py
+++ angie/__log__.py
@@ -82,3 +82,19 @@ NameError: name 'local_light' is not defined
                                                                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 13 2018 14:02:08.732 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 70
+    IMG_LIST = [C0_NORTE, C0_LESTE, C0_SUL, C0_OESTE]
+NameError: name 'C0_NORTE' is not defined
+'''},
