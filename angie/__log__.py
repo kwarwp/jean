0bf4e53545d0f63cb46f7359bc2f9992653db5d8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 11:18:22 -0300

    Created angie/__log__.py at Timestamp: 2018-08-13 14:18:21

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Aug 13 2018 11:14:18.225 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 76
+  Sala3D()
+  ^
+IndentationError: expected an indented block
+'''},
