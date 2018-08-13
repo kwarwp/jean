73a608c94321e7b0228f1d21cf0dc89c0d431393
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 19:12:58 -0300

    Saved natalia/main.py at Timestamp: 2018-08-13 22:12:58

--- natalia/main.py
+++ natalia/main.py
@@ -92,9 +92,9 @@ class Museu:
         MENSAGENS=[
                   [sala_0.norte ,"primeiramente para usar a função labirinto do vitollino precisa ter definido as salas",
               
-"""                  depois se usa a função labirinto para 5 salas, ex: labirinto(salaA, sala B, sala C, salaD, sala E),  
-                     sempre conectando uma sala a outra, exemplo: labirinto(sala E, salaF,salaG,salaH,salaI), 
-                     ficando assim: Labirinto(sala A, sala B, sala C, sala D, sala E) 
+"""                depois se usa a função labirinto para 5 salas, ex: labirinto(salaA, sala B, sala C, salaD, sala E),  
+                   sempre conectando uma sala a outra, exemplo: labirinto(sala E, salaF,salaG,salaH,salaI), 
+                   ficando assim: Labirinto(sala A, sala B, sala C, sala D, sala E) 
 """],
                                    
                   [sala_1.norte ,"Hello",  "if True:print('ooi')"],
