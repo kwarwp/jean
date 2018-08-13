5aedbc63a8b76134ec040a4c464e9bc7ded68401
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 20:02:34 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 23:02:34

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -723,3 +723,23 @@ AttributeError: 'Elemento' object has no attribute 'scorer'
     self.scorer.update(valor=cena.nome, move=self.xy,
 AttributeError: 'Elemento' object has no attribute 'nome'
 '''},
+{'date': 'Mon Aug 13 2018 19:58:30.807 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    natureza()    
+  module <module> line 174
+    colmeia = Elemento(COLMEIA, tit = "colmeia", drag= False,
+  module <module> line 61
+    self.do_drop(drop)
+TypeError: do_drop() missing 1 positional argument: y
+'''},
