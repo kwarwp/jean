562d83d3785be1f654945dd50bcef2b5a84409c8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 19:09:41 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-13 22:09:41

--- angie/__log__.py
+++ angie/__log__.py
@@ -150,3 +150,21 @@ NameError: name 'Elemento' is not defined
     Elemento(prd, style = dict(left=i*20, top =j*20, width=18, height="18px")).entra(cena)
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Mon Aug 13 2018 19:05:37.811 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 104
+    Museu()
+  module <module> line 88
+    cena = Cena(C9_OESTE)
+NameError: name 'C9_OESTE' is not defined
+'''},
