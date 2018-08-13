c4b24214d456755887d31e9bacf192ebeefdd6a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 19:47:18 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-13 22:47:18

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -679,3 +679,25 @@ AttributeError: 'method' object has no attribute 'update'
     self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
 AttributeError: 'Elemento' object has no attribute 'nome'
 '''},
+{'date': 'Mon Aug 13 2018 19:47:00.273 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 173
+    natureza()    
+  module <module> line 165
+    colmeia = Elemento(COLMEIA, tit = "colmeia", drag= False,
+  module <module> line 53
+    _ = self.entra(cena) if cena and (cena != INVENTARIO) else None
+  module _spy.vitollino.main line 456
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'Elemento' object has no attribute 'scorer'
+'''},
