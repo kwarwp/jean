8dbc90078f2cf6766463a896dceb67affc6ef700
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-14 09:44:10 -0300

    Saved grace/__log__.py at Timestamp: 2018-08-14 12:44:09

--- grace/__log__.py
+++ grace/__log__.py
@@ -271,3 +271,21 @@ ValueError: too many values to unpack (expected 3)
     [Codigo(cena = a,topo = b ,codigo= c, style= STYLE) for a, b , c in MENSAGENS]
 ValueError: too many values to unpack (expected 3)
 '''},
+{'date': 'Tue Aug 14 2018 09:44:10.334 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 222
+    Museu()
+  module <module> line 220
+    [Codigo(cena = a,topo = b ,codigo= c, style= STYLE) for a, b , c in MENSAGENS]
+ValueError: too many values to unpack (expected 3)
+'''},
