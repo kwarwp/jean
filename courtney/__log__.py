e87f40531069661a6049120b12e6f697d7dddad6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-14 09:08:46 -0300

    Saved courtney/__log__.py at Timestamp: 2018-08-14 12:08:46

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -344,3 +344,21 @@ SyntaxError: invalid syntax
     [Codigo(cena = a,topo = b ,codigo= c, style= STYLE) for a, b , c in MENSAGENS]
 ValueError: need more than 2 values to unpack
 '''},
+{'date': 'Tue Aug 14 2018 02:59:41.949 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 200
+    Museu()
+  module <module> line 192
+    [Codigo(cena = a,topo = b ,codigo= c, style= STYLE) for a, b , c in MENSAGENS]
+ValueError: need more than 2 values to unpack
+'''},
