4a4352b0ec68953df1a23eef779354d66957c1de
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 12:13:04 -0300

    Saved anastasia/__log__.py at Timestamp: 2018-08-15 15:13:04

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -24,3 +24,23 @@ NameError: name '_code_' is not defined
                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 15 2018 12:13:10.647 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 252
+    Museu()
+  module <module> line 248
+    [Codigo(cena = a,topo = b ,codigo= c, mostra = False, style= STYLE) for a, b , c in MENSAGENS]
+  module <module> line 75
+    self.elf.style.visibility = "visible" if mostra else "hidden" 
+AttributeError: 'Codigo' object has no attribute 'elf'
+'''},
