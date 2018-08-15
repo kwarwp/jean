be6db857dbd6573b5292c9dec63a82645838ec7d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 13:54:12 -0300

    Saved anastasia/__log__.py at Timestamp: 2018-08-15 16:54:11

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -112,3 +112,21 @@ SyntaxError: invalid syntax
                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 15 2018 07:45:05.983 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 252
+    Museu()
+  module <module> line 247
+    STYLE = dict(width=400,heigth="250px",left=200,top=500, bottom=100, position = absolute)
+NameError: name 'absolute' is not defined
+'''},
