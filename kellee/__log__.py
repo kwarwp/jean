2a4437111b2e7f15f210ca2a0d53dd08f2aa28b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 09:36:58 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-15 12:36:58

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -190,3 +190,21 @@ NameError: name 'heigth' is not defined
                                                                ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Aug 15 2018 09:37:04.1 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 246
+    Museu()
+  module <module> line 143
+    dino = Elemento(img=DINO, tit="Dino", style=dict(left=50, top=160, width=200, height=200))
+NameError: name 'DINO' is not defined
+'''},
