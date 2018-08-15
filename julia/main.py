6b103c23ac61d450c3024120336b384e2b99e0ec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 18:01:45 -0300

    Saved julia/main.py at Timestamp: 2018-08-15 21:01:45

--- julia/main.py
+++ julia/main.py
@@ -188,7 +188,28 @@ segunda_cena=Cena(link_para_segunda_imagem)
 primeira_cena.direita = segunda_cena
 primeira_cena.vai()
 """],
-                  #[sala_1.norte ,"Hello",  "if True:print('ooi')"],
+                  [sala_0.sul ,"Depois de aprender a acrescentar uma nova cena, você agora pode criar um cômodo completo! Utilize o código abaixo para criar as quatro cenas que formarão um cômodo inteiro.",
+                  """from _spy.vitollino.main import Cena
+link_para_primeira_imagem = "https://i.imgur.com/IPa06hM.jpg"
+link_para_segunda_imagem = "https://i.imgur.com/rHzbmtM.jpg"
+link_para_terceira_imagem = "https://i.imgur.com/NnVA765.jpg"
+link_para_quarta_imagem = "https://i.imgur.com/XJXjA9r.jpg"
+primeira_cena=Cena(link_para_primeira_imagem)
+segunda_cena=Cena(link_para_segunda_imagem)
+terceira_cena=Cena(link_para_terceira_imagem)
+quarta_cena=Cena(link_para_quarta_imagem)
+primeira_cena.direita = segunda_cena
+segunda_cena.esquerda = primeira_cena
+segunda_cena.direita = terceira_cena
+terceira_cena.esquerda = segunda_cena
+terceira_cena.direita = quarta_cena
+quarta_cena.esquerda = terceira_cena
+quarta_cena.direita = primeira_cena
+primeira_cena.esquerda = quarta_cena
+primeira_cena.vai()
+"""],
+                  
+                  [sala_1.norte ,"Hello",  "if True:print('ooi')"],
                   [sala_1.leste ,"bonju",  "if True:print(':)')"],
                   [sala_1.oeste ,"Buenos dias",  "if True:print('oi')"],
                   [sala_1.sul ,"como vai?",  "if True:print('oi')"],
