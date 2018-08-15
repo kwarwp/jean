bdefb1cf7c70eb563e4caed9b13f4b35cca81f66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 12:25:56 -0300

    Saved julia/main.py at Timestamp: 2018-08-15 15:25:56

--- julia/main.py
+++ julia/main.py
@@ -174,14 +174,18 @@ class Museu:
        # cod = Codigo(cena=sala_1.oeste, topo = "Buenos dias",codigo = "if True:print('ooi')",style = dict(width=400,heigth="250px",left=500,top=100))
        # cod =Codigo(cena=sala_1.sul, topo = "como vai?",codigo = "if True:print('ooi')",style = dict(width=400,heigth="250px",left=500,top=100))
         
-        MENSAGENS=[
-                  [sala_0.norte ,"Agora que você já conseguiu programar sua primeira cena, que tal acrescentar uma nova e poder navegar entre elas com um clique no meio da imagem?"
+        MENSAGENS=[sala_0.norte ,"Olá! Se prepare, que finalmente chegou o dia de aprender a programar em Python. Vamos embarcar nessa aventura já! Crie uma cena utilizando o código abaixo. ",
+                  """from _spy.vitollino.main import Cena
+link_para_uma_imagem = "https://i.imgur.com/IPa06hM.jpg" 
+uma_cena=Cena(link_para_imagem)
+uma_cena.vai""",
+                  sala_1.leste ,"Agora que você já conseguiu programar sua primeira cena, que tal acrescentar uma nova e poder navegar entre elas com um clique no meio da imagem?",
                   """from _spy.vitollino.main import Cena
 link_para_primeira_imagem = "https://i.imgur.com/IPa06hM.jpg"
 link_para_segunda_imagem = "https://i.imgur.com/rHzbmtM.jpg"
 primeira_cena=Cena(link_para_primeira_imagem)
 segunda_cena=Cena(link_para_segunda_imagem)
-primeira_cena.meio = segunda_cena
+primeira_cena.direita = segunda_cena
 primeira_cena.vai()
 """],
                   [sala_1.norte ,"Hello",  "if True:print('ooi')"],
