071b3ed71dec4b7a121d49d5be1451fd32b4a6e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-16 05:11:39 -0300

    Saved adda/main.py at Timestamp: 2018-08-16 08:11:38

--- adda/main.py
+++ adda/main.py
@@ -1,34 +1,32 @@
 # jean.adda.main.py
 MAPA = "https://i.imgur.com/d9YNdTu.png"
 
-from _spy.vitollino.main import Sala, STYLE, Codigo, Labirinto
+from _spy.vitollino.main import Sala, STYLE, Cena, Labirinto
 
 STYLE["width"] = 850
 STYLE["height"] = "650px"
 
 
-class Museu:
-
-    def __init__(self):  
-        
-        MAPA.norte.vai()
-
-             
-        
-'''if __name__ == '__main__':
-    #Mapa()
-    Museu()
-    
 class Mapa:
-    def __init__(self, c=NADA, n=NADA, l=NADA, s=NADA, o=NADA):
-        self.salas = [sala for sala in [c, n, l, s, o]]
-        self.centro, self.norte, self.leste, self.sul, self.oeste = self.salas
-        for indice, sala in enumerate(self.salas[1:]):
-            self.centro.cenas[indice].portal(N=sala.cenas[indice])
-            indice_oposto = (indice + 2) % 4
-            sala.cenas[indice_oposto].portal(N=self.centro.cenas[indice_oposto])
+    def __init__(self):
+    self.cena_mapa = Cena(img = MAPA)
+        
+    self.cena_mapa.meio = Cena(vai=self.vai_importar)
+    self.cena_mapa.vai()
+
+def vai(self, *_):
+    self.cena_mapa.vai()
     
-    def __criar_conexao_salas(m):
-        pass #não sei como reutilizar a função mapa :(
+    
+    
+    def vai_importar(self, *_):
+    from Jean.Kellee.main import Museu
     Museu()
+    
+def vai_CenaTutorialInterativo():
+    cenaImporta = CenaTutorialInterativo()
+    cenaImporta.vai()
+
+if __name__ == "__main__":
+	vai_CenaTutorialInterativo()
                            
