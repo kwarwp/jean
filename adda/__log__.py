5c007a8d605004475447d16b15c667e1d59a7fa1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-16 05:29:41 -0300

    Saved adda/__log__.py at Timestamp: 2018-08-16 08:29:41

--- adda/__log__.py
+++ adda/__log__.py
@@ -167,3 +167,23 @@ AttributeError: 'module' object has no attribute '__setitem__'
     STYLE["height"] = "560px"
 AttributeError: 'module' object has no attribute '__setitem__'
 '''},
+{'date': 'Thu Aug 16 2018 05:29:41.744 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    	vai_CenaMAPA()
+  module <module> line 26
+    cenaImporta = Mapa()
+  module <module> line 11
+    self.cena_mapa = Cena(img = MAPA)
+TypeError: 'module' object is not callable
+'''},
