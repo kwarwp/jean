a444163b92c18d274f3320fa5da5ba8a7769d8e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-16 22:31:22 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-17 01:31:22

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -631,3 +631,21 @@ AttributeError: 'list' object has no attribute 'vai'
     MENSAGENS.vai=STYLE = dict(width=400,heigth="250px",left=400,top=100, visibility= "visible", opacity= 1)
 AttributeError: 'list' object has no attribute 'vai'
 '''},
+{'date': 'Thu Aug 16 2018 22:34:18.375 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 255
+    Museu()
+  module <module> line 250
+    MENSAGENS.vai=STYLE = dict(width=400,heigth="250px",left=400,top=100, visibility= "visible", opacity= 1)
+AttributeError: 'list' object has no attribute 'vai'
+'''},
