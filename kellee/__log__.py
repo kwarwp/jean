7a6ad75c3b5806dc1f70f7f4de12d54b9ec76445
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-16 22:25:37 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-17 01:25:37

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -577,3 +577,21 @@ UnboundLocalError: local variable 'MENSAGENS' referenced before assignment
     interrogacao.vai=MENSAGENS.vai
 UnboundLocalError: local variable 'MENSAGENS' referenced before assignment
 '''},
+{'date': 'Thu Aug 16 2018 22:28:33.687 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 254
+    Museu()
+  module <module> line 250
+    interrogacao.vai=MENSAGENS.vai
+AttributeError: 'list' object has no attribute 'vai'
+'''},
