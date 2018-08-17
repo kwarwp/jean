abe8635790bd5410505999ef0e8e37fc841c5623
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-16 22:05:48 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-17 01:05:48

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -541,3 +541,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Aug 16 2018 22:08:44.219 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 254
+    Museu()
+  module <module> line 147
+    MENSAGENS
+UnboundLocalError: local variable 'MENSAGENS' referenced before assignment
+'''},
