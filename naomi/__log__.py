a4289991a93d8869dfd3ef43f2defeba202837dc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-17 00:14:43 -0300

    Saved naomi/__log__.py at Timestamp: 2018-08-17 03:14:43

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -125,3 +125,21 @@ AttributeError: 'NoneType' object has no attribute '_code_'
     INVENTARIO.cena._code_.elt.style={"visibility": "hidden", "opacity": 0}
 AttributeError: 'Cena' object has no attribute '_code_'
 '''},
+{'date': 'Fri Aug 17 2018 00:15:11.118 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 258
+    Museu()
+  module <module> line 250
+    INVENTARIO.cena._code_.elt.style={"visibility": "hidden", "opacity": 0}
+AttributeError: 'Cena' object has no attribute '_code_'
+'''},
