c9474af606e4b5131ce1fe27ce23d66b81d5839a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-20 08:34:30 -0300

    Saved sara/__log__.py at Timestamp: 2018-08-20 11:34:29

--- sara/__log__.py
+++ sara/__log__.py
@@ -95,3 +95,25 @@ AttributeError: 'Museu' object has no attribute 'nome'
     cena <= self
 TypeError: '<=' not supported between instances of 'Museu' and 'Codigo'
 '''},
+{'date': 'Mon Aug 20 2018 08:34:30.611 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    cena.vai()
+  module stacy.main line 104
+    Museu.CUR.instrumenta(self)
+  module stacy.main line 100
+    self.cod.entra(cena)
+  module _spy.vitollino.main line 459
+    cena <= self
+TypeError: '<=' not supported between instances of 'Museu' and 'Codigo'
+'''},
