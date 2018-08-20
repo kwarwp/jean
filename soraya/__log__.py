a085019700884a81f00e41086ca19b775c2c66a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-20 10:49:53 -0300

    Saved soraya/__log__.py at Timestamp: 2018-08-20 13:49:52

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -51,3 +51,21 @@ NameError: name 'PEDRA' is not defined
     pedra = Elemento(img=PEDRA, tit="Pedra", style=dict(left=40, top=380, width=200, height="150px"))
 NameError: name 'PEDRA' is not defined
 '''},
+{'date': 'Mon Aug 20 2018 10:49:32.697 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 334
+    Museu()
+  module <module> line 324
+    [Codigo(cena = a,topo = b ,codigo= c, style= STYLE) for a, b , c in MENSAGENS]
+ValueError: need more than 2 values to unpack
+'''},
