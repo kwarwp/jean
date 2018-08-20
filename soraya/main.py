8f17730acd14464994b27fbf879cce34deb8c230
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-20 11:01:19 -0300

    Saved soraya/main.py at Timestamp: 2018-08-20 14:01:18

--- soraya/main.py
+++ soraya/main.py
@@ -264,6 +264,7 @@ pedra.vai
                   [sala_8.sul,"está quase acabando",  "if True:print('oi')"],
                   [sala_9.norte,"você esta vencendo seus medos",  "if True:print('oi')"],
                   [sala_9.leste ,"Agora precisa definir cada sala que tem 4 cenas ou paredes, para dfinir cada sala se utliza cenas"
+                                 " Esaas salas servem para vc ter um cômodo onde se olha todas as paredes"
                                 "por exemplo,salaA(Cena1, Cena2,Cena3,Cena4), sala B( Cena5,Cena6,Cena7, Cena8), sala C(Cena9, Cena10, Cena11, Cena12)"
                                 " e assim vai secessivamente ate criar a quantidade de salas que achar necessário, para criar seu jogo"
     """ sala_0 = Sala(R_NORTE, R_LESTE, R_SUL, R_OESTE)
