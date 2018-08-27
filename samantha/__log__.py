9c0dc953dac7e1b0fe077ab31bc00363b9e925ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-27 15:10:17 -0300

    Saved samantha/__log__.py at Timestamp: 2018-08-27 18:10:17

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -77,3 +77,21 @@ NameError: name 'Elemento' is not defined
     colmeia = Elemento(COLMEIA, tit = "colmeia", drag= False,
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Mon Aug 27 2018 15:09:55.424 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    natureza()    
+  module <module> line 18
+    colmeia = Elemento(COLMEIA, tit = "colmeia", drag= False,
+NameError: name 'Elemento' is not defined
+'''},
