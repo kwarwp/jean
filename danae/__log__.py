d6f72674f1e28f9f6b87e2622c00762daf4c662a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 20:24:38 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-28 23:24:38

--- danae/__log__.py
+++ danae/__log__.py
@@ -124,3 +124,10 @@ SyntaxError: invalid syntax
                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Aug 28 2018 20:24:16.443 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 60
+  for x, tit in enumerate(SALAS)]
+                                ^
+SyntaxError: invalid syntax
+'''},
