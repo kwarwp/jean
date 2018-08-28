8f41ac8aba7ee72ad1879c5da2d1259025e7ecf9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 17:47:38 -0300

    Saved soraya/main.py at Timestamp: 2018-08-28 20:47:37

--- soraya/main.py
+++ soraya/main.py
@@ -262,7 +262,7 @@ pedra.vai
                   [sala_8.leste ,"vc é capaz",  "if True:print('oi')"],
                   [sala_8.oeste ," be free",  "if True:print('oi')"],
                   [sala_8.sul,"está quase acabando",  "if True:print('oi')"],
-                  [sala_9.norte,"Antes de criar as salas precisa criar um inicio de uma função para você colocar as salas, é como se fosse a casa que as slas estaram inseridas, pois sem casa ou pŕedio não se tem salas...então o código abaixo mostra onde as salas tem que estar inseridas.", 
+                  [sala_9.norte,"Antes de criar as salas precisa criar um inicio de uma função para você colocar as salas, é como se fosse a casa que as slas estaram inseridas, pois sem casa ou pÅedio não se tem salas...então o código abaixo mostra onde as salas tem que estar inseridas.", 
 """class Museu:
      def __init__(self):
 """],
@@ -295,7 +295,12 @@ pedra.vai
         Labirinto(sala A, sala B, sala C, sala D, sala E)
         Labirinto(sala E, sala F, sala G, sala H, sala I)
 """],
-                  [sala_A.norte ,"tudo vai bem",  "if True:print('oi')"],
+                  [sala_A.norte ,"Para facilitar mais e não precisar conectar uma sala a outra vc usa a função Labirito.m e coloca "[" entre 4 salas que assim irá juntar todas as salas do labirinto como se fosse um mapa",  
+"""
+Labirinto.m  ([[sala_0, sala_1, sala_2, sala_3],[ sala_4, sala_5, sala_6, sala_7],
+               [ sala_8, sala_9, sala_A, sala_B],[ sala_C, sala_D, sala_E]])
+        
+"""],
                   [sala_A.leste ,"não desista",  "if True:print('oi')"],
                   [sala_A.oeste ,"não é tão complicado",  "if True:print('oi')"],
                   [sala_A.sul ,"avai sim",  "if True:print('oi')"],
