6b354d062adf04d56cf186999fc08709ff284094
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 16:03:31 -0300

    Saved sarah/__log__.py at Timestamp: 2018-08-28 19:03:31

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -68,3 +68,19 @@ NameError: name 'sala_0' is not defined
     sala_0.norte.vai()
 NameError: name 'sala_0' is not defined
 '''},
+{'date': 'Tue Aug 28 2018 16:02:29.212 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    sala_0.norte.vai()
+NameError: name 'sala_0' is not defined
+'''},
