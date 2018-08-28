30f1cadf404a31d94002e983e735702a203670a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 16:05:02 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-08-28 19:05:02

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -79,3 +79,21 @@ Traceback (most recent call last):
   module <module> line 2
     from _spy.vitollino.main import Mapa, Sala, STYLE, Codigo as Code, INVENTARIO, Elemento, NS,Texto
 '''},
+{'date': 'Tue Aug 28 2018 16:04:40.118 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 94
+    Museu()
+  module <module> line 90
+    labirinto.m  (sala_0, sala_1, sala_2, sala_3, sala_4, sala_5, sala_6, sala_7, sala_8, sala_9, sala_A, sala_B, sala_C, sala_D, Sala_E)
+NameError: name 'labirinto' is not defined
+'''},
