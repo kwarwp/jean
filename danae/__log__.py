7a3a1b70827344a6fca993566c440423784c9834
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 20:41:13 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-28 23:41:13

--- danae/__log__.py
+++ danae/__log__.py
@@ -215,3 +215,21 @@ Exception: x is undefined
   module '$exec_2035' line 68
 inventario()
 '''},
+{'date': 'Tue Aug 28 2018 20:40:50.156 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    inventario()
+  module <module> line 58
+    marcador.vai= cenas[x].vai
+IndexError: list index out of range
+'''},
