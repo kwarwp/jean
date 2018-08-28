c1a079785013b6433bf1608cdf213addfb138b79
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 19:01:34 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-28 22:01:33

--- danae/__log__.py
+++ danae/__log__.py
@@ -92,3 +92,21 @@ SyntaxError: invalid syntax
                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Aug 28 2018 19:01:11.124 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    inventario()
+  module <module> line 12
+    INVENTARIO.bota.elemento
+AttributeError: 'method' object has no attribute 'elemento'
+'''},
