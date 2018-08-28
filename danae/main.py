72c58b709f39086dc020f0413ac235efcb80e0a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 19:21:55 -0300

    Saved danae/main.py at Timestamp: 2018-08-28 22:21:55

--- danae/main.py
+++ danae/main.py
@@ -6,6 +6,7 @@ STYLE["width"] = 850
 STYLE["height"] = "650px"
 MAPA = "https://i.imgur.com/d9YNdTu.png"
 R_OESTE = "https://i.imgur.com/XJXjA9r.jpg"
+CIRCULOB = "https://images.emojiterra.com/twitter/512px/26aa.png"
 
 def inventario():
     mapa = Elemento(img=MAPA,tit= "Mapa",style=dict(left=50, top=160, width=200, height="200px"))
