cccf76e5be4d62e2969a150df741d74e0da14162
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 16:16:50 -0300

    Saved kathryn/main.py at Timestamp: 2018-08-28 19:16:50

--- kathryn/main.py
+++ kathryn/main.py
@@ -87,7 +87,7 @@ class Museu:
         sala_C = Sala(CC_NORTE, CC_LESTE, CC_SUL, CC_OESTE)
         sala_D = Sala(CD_NORTE, CD_LESTE, CD_SUL, CD_OESTE)
         sala_E = Sala(CE_NORTE, CE_LESTE,CE_SUL, CE_OESTE)
-        Labirinto.m  (sala_0, sala_1, sala_2, sala_3, sala_4, sala_5, sala_6, sala_7, sala_8, sala_9, sala_A, sala_B, sala_C, sala_D, Sala_E)
+        Labirinto.m  ([[sala_0, sala_1, sala_2, sala_3],[ sala_4, sala_5, sala_6, sala_7],[ sala_8, sala_9, sala_A, sala_B],[ sala_C, sala_D, sala_E]])
         
         
 if __name__ == '__main__':
