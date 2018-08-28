fb2d284a473c85871a33fd5764cb510bb3cb31ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 16:00:27 -0300

    Created sarah/__log__.py at Timestamp: 2018-08-28 19:00:27

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 28 2018 15:59:23.135 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  sala_0 = Sala(R_NORTE, R_LESTE, R_SUL, R_OESTE)
+  ^
+IndentationError: unexpected indent
+'''},
