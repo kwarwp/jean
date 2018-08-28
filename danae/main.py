e4681791b2fba47a5aa9c7ebce04c8671f5d7e4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 20:39:02 -0300

    Saved danae/main.py at Timestamp: 2018-08-28 23:39:02

--- danae/main.py
+++ danae/main.py
@@ -22,6 +22,21 @@ Uso do Elemento no Inventário
 Drag and drop do Elemento para a cena
 """.split("\n")
 #Drag and drop do Elemento para outro elemento
+SALAS = dict (R_OESTE = "https://i.imgur.com/XJXjA9r.jpg",
+CA_OESTE = "https://i.imgur.com/ozE3Qhg.jpg",
+C3_SUL = "https://i.imgur.com/gIvRKAJ.jpg",
+C4_SUL = "https://i.imgur.com/xnD8ZtB.jpg",
+C5_SUL = "https://i.imgur.com/jTAz1ND.jpg",
+C6_SUL = "https://i.imgur.com/MBVBvDM.jpg",
+CB_LESTE = "https://i.imgur.com/iSGp4fP.jpg",
+CC_SUL = "https://i.imgur.com/M61ZyN8.jpg",
+CD_SUL = "https://i.imgur.com/LakLTda.jpg",
+CE_SUL = "https://i.imgur.com/uhY89Y6.jpg",
+C7_SUL = "https://i.imgur.com/AXT4Zcl.jpg",
+C8_OESTE = "https://i.imgur.com/R3FtDb2.jpg",
+)
+
+
 
 def inventario():
     mapa = Elemento(img=MAPA,tit= "Mapa",style=dict(left=50, top=160, width=200, height="200px"))
@@ -31,14 +46,19 @@ def inventario():
     INVENTARIO.bota(mapa)
     cmapa= Cena(img=MAPA)
     mapa.vai = cmapa.vai
+    cenas= [Cena(img=cena) for cena in SALAS.values()]
     dx, dy = 850//6, 650//2
     marcadores= [Elemento (img= CIRCULOB, tit=tit, style=dict(
         left=dx*(x%6), top=dy*(x//6), width=dx, height="325px", opacity=0.01,
         ))
+       
     for x, tit in enumerate (CENAS)]
     for marcador in marcadores :
         marcador.entra(cmapa)
-        #marcador.vai 
+        marcador.vai
+        
+        
+
     
     
     
@@ -46,3 +66,15 @@ def inventario():
     
     r_oeste.vai()
 inventario()
+
+
+""" csalas= Cena(img= SALAS)
+    sala.vai = csala.vai  
+    marcadores= [Elemento (img= CIRCULOB, tit=tit, style=dict(
+        left=dx*(x%6), top=dy*(x//6), width=dx, height="325px", opacity=0.01,
+        ))
+    for x, tit in enumerate(SALAS)]
+    for marcador in marcadores:
+        marcador.entra(SALAS)
+        marcador.vai 
+"""
