8f637d54319db3cdf46f378d29688c9671b449c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 18:47:42 -0300

    Created danae/main.py at Timestamp: 2018-08-28 21:47:42

---
+++ danae/main.py
@@ -0,0 +1,7 @@
+# jean.danae.main.py
+from _spy.vitollino.main import Sala, STYLE, Codigo as Code, INVENTARIO, Elemento, NS,Texto
+from browser import html
+
+STYLE["width"] = 850
+STYLE["height"] = "650px"
+MAPA = "https://i.imgur.com/d9YNdTu.png"
