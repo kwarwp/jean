133084e7dbf62dda9a743586d0840cff061c14c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 15:55:42 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-08-28 18:55:42

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -17,3 +17,34 @@
     sala_0 = Sala(R_NORTE, R_LESTE, R_SUL, R_OESTE)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Tue Aug 28 2018 15:55:19.345 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from _spy.vitollino.main import Mapa, Sala, STYLE, Codigo as Code, INVENTARIO, Elemento, NS,Texto
+ImportError: cannot import name 'Mapa'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from _spy.vitollino.main import Mapa, Sala, STYLE, Codigo as Code, INVENTARIO, Elemento, NS,Texto
+'''},
