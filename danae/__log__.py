14cfcc7f2d682d63b1e327fed2bada90994f85e8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 20:36:20 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-28 23:36:19

--- danae/__log__.py
+++ danae/__log__.py
@@ -177,3 +177,21 @@ SyntaxError: invalid syntax
     sala.vai = csala.vai  
 NameError: name 'sala' is not defined
 '''},
+{'date': 'Tue Aug 28 2018 20:35:57.28 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 75
+    inventario()
+  module <module> line 61
+    sala.vai = csala.vai  
+NameError: name 'sala' is not defined
+'''},
