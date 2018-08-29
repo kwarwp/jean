fd1547fc8163d9e7d0ca82630f4032facbaa43f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 23:16:19 -0300

    Saved sarah/__log__.py at Timestamp: 2018-08-29 02:16:19

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -181,3 +181,21 @@ NameError: name 'none' is not defined
     Labirinto(sala_2, sala_1, sala_3, none, None)
 NameError: name 'none' is not defined
 '''},
+{'date': 'Tue Aug 28 2018 23:16:22.406 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 102
+    Museu()
+  module <module> line 91
+    dino = Elemento(img=DINO, tit="Dino", style=dict(left=50, top=160, width=200, height=200))
+NameError: name 'Elemento' is not defined
+'''},
