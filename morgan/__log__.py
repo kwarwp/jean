02e0217df67bb85833b865b477b4ab78553a8dbb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 15:00:50 -0300

    Saved morgan/__log__.py at Timestamp: 2018-08-30 18:00:50

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -6,3 +6,10 @@
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Aug 30 2018 15:00:49.668 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 74
+  sala_1 = Sala(S1_NORTE, S1_LESTE, S1_SUL, S1_OESTE)
+  ^
+IndentationError: unexpected indent
+'''},
