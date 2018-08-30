bb60707325e6af9f2fd8ac6ebb429bb12ebff86a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 15:15:36 -0300

    Saved morgan/__log__.py at Timestamp: 2018-08-30 18:15:36

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -27,3 +27,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Aug 30 2018 15:15:35.339 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    class Museu:
+  module <module> line 86
+    sala_0.leste.vai()
+NameError: name 'sala_0' is not defined
+'''},
