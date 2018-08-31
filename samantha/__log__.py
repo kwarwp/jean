d14c959e8510982126f85900c620b746457a610e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 21:47:49 -0300

    Saved samantha/__log__.py at Timestamp: 2018-08-31 00:47:49

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -364,3 +364,21 @@ SyntaxError: invalid syntax
                                                  ^
 SyntaxError: keyword argument repeated
 '''},
+{'date': 'Thu Aug 30 2018 21:47:25.839 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 197
+    teste()  
+  module <module> line 191
+    Codigo._area.style.overflow = "scroll"
+AttributeError: 'Codigo' object has no attribute '_area'
+'''},
