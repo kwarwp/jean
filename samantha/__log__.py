65c11cb02b2efbbe9eb471013f9682e9d4069f13
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 21:54:54 -0300

    Saved samantha/__log__.py at Timestamp: 2018-08-31 00:54:54

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -400,3 +400,21 @@ AttributeError: 'Codigo' object has no attribute '_area'
     self._area.style.overflow="scroll"
 AttributeError: 'GUI' object has no attribute '_area'
 '''},
+{'date': 'Thu Aug 30 2018 21:54:31.413 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 211
+    teste()  
+  module <module> line 205
+    self._area.style.overflow="scroll"
+AttributeError: 'GUI' object has no attribute '_area'
+'''},
