8c65984c338a55702506258a20233ee3a8204f36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 21:23:09 -0300

    Saved alexa/__log__.py at Timestamp: 2018-08-31 00:23:09

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -750,3 +750,21 @@ TypeError: do_drop() missing 1 positional argument: y
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Aug 30 2018 21:22:46.88 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 192
+    natureza()  
+  module <module> line 187
+    [Codigo(cena = a,topo = b ,codigo= c, style= STYLE) for a, b , c in MENSAGENS]
+NameError: name 'Codigo' is not defined
+'''},
