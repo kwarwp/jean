8cdce118c1d740547577c28a9d90c26f5bbe9718
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-03 14:46:14 -0300

    Saved danae/__log__.py at Timestamp: 2018-09-03 17:46:13

--- danae/__log__.py
+++ danae/__log__.py
@@ -251,3 +251,21 @@ IndexError: list index out of range
     cenas = [museu.sala_0.norte,sala_0.leste,sala_0.sul,sala_0.oeste, sala_5.norte,sala_5.leste, sala_7.oeste,sala_9.oeste,sala_9.norte,sala_9.leste,sala_9.sul ,sala_A.leste]
 AttributeError: 'Museu' object has no attribute 'sala_0'
 '''},
+{'date': 'Mon Sep 03 2018 14:45:48.633 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 69
+    inventario()
+  module <module> line 52
+    cenas = [museu.sala_0.norte,sala_0.leste,sala_0.sul,sala_0.oeste, sala_5.norte,sala_5.leste, sala_7.oeste,sala_9.oeste,sala_9.norte,sala_9.leste,sala_9.sul ,sala_A.leste]
+NameError: name 'sala_0' is not defined
+'''},
