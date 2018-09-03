64cf715d646c8f22ab3180b09b54694d283920d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-03 12:52:10 -0300

    Saved soraya/main.py at Timestamp: 2018-09-03 15:52:10

--- soraya/main.py
+++ soraya/main.py
@@ -399,8 +399,9 @@ from _spy.vitollino.main import Sala, Labirinto
         Labirinto(sala_11, None, sala_10, None, sala_12)
 """],
                   [sala_A.norte ,"não desista",  "if True:print('oi')"],
-                  [sala_A.leste ,"Agora para ficar mais divertido temos como mexer os elementos na Cena, chamamos esse movimento de Drag and Drop ",  
-"""  VAR1 = 'https://i.imgur.com/vb4zrA0.jpg'
+                  [sala_A.leste ,"não desista",  
+"""
+VAR1 = 'https://i.imgur.com/vb4zrA0.jpg'
 VAR2 = 'https://trabalhodigitalinfo.files.wordpress.com/2016/01/seta-vermelha-2png.png'
 VAR3 = "https://imagens.simplo7.net/static/2497/sku/thumb_tricoline-100-algodao-lisa-tricoline-100-algodao-lisa-branca-1474467553683.jpg"
 
@@ -421,7 +422,7 @@ def movimento():
     camada1.vai()
 
 movimento() 
-"""],
+"""],,
                   [sala_A.oeste ,"não é tão complicado",  "if True:print('oi')"],
                   [sala_A.sul ,"avai sim",  "if True:print('oi')"],
                   [sala_B.norte ,"tudo vai bem",  "if True:print('oi')"],
