f67256174b5e9d2735e34896b2a24c128077cc61
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 19:02:33 -0300

    Saved meredith/__log__.py at Timestamp: 2018-09-24 22:02:32

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -435,3 +435,21 @@ IndentationError: unexpected indent
     self.scorer.update(valor=cena.nome, move=self.xy,
 AttributeError: 'str' object has no attribute 'nome'
 '''},
+{'date': 'Mon Sep 24 2018 19:01:43.676 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 117
+    dino.entra(S7_NORTE)
+  module _spy.vitollino.main line 462
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'str' object has no attribute 'nome'
+'''},
