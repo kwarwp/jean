edd1a0787f98cedc1f3e6e3af138f72cd2546faf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 17:14:27 -0300

    Saved meredith/__log__.py at Timestamp: 2018-09-24 20:14:27

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -313,3 +313,21 @@ NameError: name 'sala_7' is not defined
     dino.entra(sala_7.norte)
 NameError: name 'sala_7' is not defined
 '''},
+{'date': 'Mon Sep 24 2018 17:13:38.336 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 127
+    Museu()
+  module <module> line 92
+    sala_1 = Sala(S1_NORTE, S1_LESTE, S1_SUL, S1_OESTE)
+UnboundLocalError: local variable 'S1_SUL' referenced before assignment
+'''},
