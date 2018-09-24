e7ad3021b5e1a9e76c9f0b7d4373ab653c300cf5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 11:25:01 -0300

    Created meredith/__log__.py at Timestamp: 2018-09-24 14:25:01

---
+++ meredith/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 24 2018 11:24:11.549 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 106
+  sala_0 = Sala(S0_NORTE, S0_LESTE, S0_SUL, S0_OESTE)
+  ^
+IndentationError: unexpected indent
+'''},
