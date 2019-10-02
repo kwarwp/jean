e7f3d917e4059123b1c8dd9022f4cedbe20c4924
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 14:23:00 -0300

    Saved soraya/__log__.py @2019-10-02 17:23

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -309,3 +309,23 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Oct 02 2019 14:22:57.23 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 463
+    Museu()
+  module <module> line 169
+    INVENTARIO.bota(interrogacao)
+  module _spy.vitollino.main line 360
+    nome = nome_item.tit or nome_item
+AttributeError: 'Elemento' object has no attribute 'tit'
+'''},
