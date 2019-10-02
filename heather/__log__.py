344dbbdb74bbe88e9ce92c8bf0586351e944b436
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 14:24:56 -0300

    Saved heather/__log__.py @2019-10-02 17:24

--- heather/__log__.py
+++ heather/__log__.py
@@ -15,3 +15,23 @@
     uma_cena=Cena(link_para_imagem)
 NameError: name 'link_para_imagem' is not defined
 '''},
+{'date': 'Wed Oct 02 2019 14:24:55.464 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 438
+    Museu()
+  module <module> line 170
+    INVENTARIO.bota(interrogacao)
+  module _spy.vitollino.main line 360
+    nome = nome_item.tit or nome_item
+AttributeError: 'Elemento' object has no attribute 'tit'
+'''},
