d71a8afc7b418d0f5095c1e8567447190af92e76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 14:03:21 -0300

    Saved rachel/__log__.py @2019-10-02 17:03

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -17,3 +17,23 @@
     video=Video(source="https://www.w3schools.com/html/mov_bbb.mp4",width="320", height="240",top=0, 
 NameError: name 'Video' is not defined
 '''},
+{'date': 'Wed Oct 02 2019 14:03:20.557 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 449
+    Museu()
+  module <module> line 158
+    INVENTARIO.bota(interrogacao)
+  module _spy.vitollino.main line 360
+    nome = nome_item.tit or nome_item
+AttributeError: 'Elemento' object has no attribute 'tit'
+'''},
