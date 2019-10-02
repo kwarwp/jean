8116d939d63ab52534efd9696af9d2f51b86a5c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 14:29:31 -0300

    Saved danae/__log__.py @2019-10-02 17:29

--- danae/__log__.py
+++ danae/__log__.py
@@ -461,3 +461,25 @@ AttributeError: 'Elemento' object has no attribute 'tit'
     nome = nome_item.tit or nome_item
 AttributeError: 'Elemento' object has no attribute 'tit'
 '''},
+{'date': 'Wed Oct 02 2019 14:29:29.875 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 72
+    inventario()
+  module <module> line 45
+    museu = Museu()
+  module rachel.main line 158
+    INVENTARIO.bota(interrogacao)
+  module _spy.vitollino.main line 360
+    nome = nome_item.tit or nome_item
+AttributeError: 'Elemento' object has no attribute 'tit'
+'''},
